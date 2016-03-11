516034ad42e321e3f0cf8a028cf84af2f8edef09
Jellyfish <1129088994@qq.com>
2016-03-11 17:43:03 +0800

    git commit

---
+++ javascripts/script.js
@@ -0,0 +1,15 @@
+new FullPage({
+    id: "pageContain",
+    slideTime: 800,
+    continuous: false,
+    effect: {
+        transform: {
+            translate: 'Y',
+            scale: [1, 1],
+            rotate: [0, 0]
+        },
+        opacity: [0, 1]
+    },
+    mode: 'wheel,touch,nav:navBar',
+    easing: 'ease'
+});
