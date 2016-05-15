ca86755cf8cdcef0717a76751f7ae166bd0a1c1e
Jellyfish <1129088994@qq.com>
2016-05-15 14:08:40 +0800

    imgsbed move to qiniu

--- mission_43/src/app.js
+++ mission_43/src/app.js
@@ -1,8 +1,9 @@
 var util = require('./util');
 var Displayer = require('./Displayer');
 
-var count = 1;
-
-for(var i = 1; i <= count; i++){
-    document.body.appendChild(new Displayer(240, 180, util.getimgs(i, 16, './imgsbed/')).show());
+var count = 1,
+    baseUrl = "http://7xq0r0.com1.z0.glb.clouddn.com/m43_imgsbed/";
+    
+for (var i = 1; i <= count; i++) {
+    document.body.appendChild(new Displayer(240, 180, util.getimgs(i, 16, baseUrl)).show());
 }
